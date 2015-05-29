9cd4dd6de7e266ee9e8b0f739c2969d812a8119e
mperez14 <mperez14@nd.edu>
2015-05-29 14:57:54 -0400

    Initial Commit

---
+++ email/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  email
+//
+//  Created by Matthew Perez on 5/29/15.
+//  Copyright (c) 2015 Matthew Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ email/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  email
+//
+//  Created by Matthew Perez on 5/29/15.
+//  Copyright (c) 2015 Matthew Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
